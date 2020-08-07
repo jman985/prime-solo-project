49cef67737187bb0323a43e66e7f06a748f49d2d
Josh Maus <jman985@gmail.com>
2020-08-07 10:56:27 -0500

    adjusted querys with new database

--- server/routes/build.router.js
+++ server/routes/build.router.js
@@ -12,57 +12,85 @@ router.get('/:id', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
     console.log('req.params:', req.params.id);
 
-    pool.query(`SELECT "build"."id" AS build_id,
-    "user_id",
-    cpu.id AS cpu_id,
-    cpu.name AS cpu_name,
-    cpu.image AS cpu_image,
-    cpu.details AS cpu_details,
-    
-    cooler.id AS cooler_id,
-    cooler.name AS cooler_name,
-    cooler.image AS cooler_image,
-    cooler.details AS cooler_details,
-    
-    motherboard.id AS mobo_id,
-    motherboard.name AS mobo_name,
-    motherboard.image AS mobo_image,
-    motherboard.details AS mobo_details,
-    
-    storage.id AS storage_id,
-    storage.name AS storage_name,
-    storage.image AS storage_image,
-    storage.details AS storage_details,
-    
-    gpu.id AS gpu_id,
-    gpu.name AS gpu_name,
-    gpu.image AS gpu_image,
-    gpu.details AS gpu_details,
-    
-    "case".id AS case_id,
-    "case".name AS case_name,
-    "case".image AS case_image,
-    "case".details AS case_details,
-    
-    memory.id AS memory_id,
-    memory.name AS memory_name,
-    memory.image AS memory_image,
-    memory.details AS memory_details,
-    
-    psu.id AS psu_id,
-    psu.name AS psu_name,
-    psu.image AS psu_image,
-    psu.details AS psu_details
-    
+    pool.query(`SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
     FROM "build"
-    JOIN "cpu" ON "cpu_id" = "cpu"."id" 
-    JOIN "motherboard" ON "mobo_id" = "motherboard"."id"
-    JOIN "cooler" ON "cooler_id" = "cooler"."id"
-    JOIN "gpu" ON "gpu_id" = "gpu"."id"
-    JOIN "case" ON "case_id" = "case"."id"
-    JOIN "storage" ON "storage_id" = "storage"."id"
-    JOIN "memory" ON "memory_id" = "memory"."id"
-    JOIN "psu" ON "psu_id" = "psu"."id" WHERE build.id= $1;`, [req.params.id])
+    JOIN "components" ON "cpu_id" = "components"."id" WHERE user_id = $1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "cooler_id" = "components"."id" WHERE user_id = $1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "mobo_id" = "components"."id" WHERE user_id = $1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "case_id" = "components"."id" WHERE user_id = $1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "gpu_id" = "components"."id" WHERE user_id = 1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "storage_id" = "components"."id" WHERE user_id = $1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "memory_id" = "components"."id" WHERE user_id = $1 AND build.id = $2
+    UNION
+    SELECT
+    build.name AS build_name,
+    components.id AS id,
+    components.type AS type,
+    components.name AS name,
+    components.image AS image,
+    components.description AS description
+    FROM "build"
+    JOIN "components" ON "psu_id" = "components"."id" WHERE user_id = $1 AND build.id = $2 ORDER BY id;`, [req.user.id, req.params.id])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for components:', error);
