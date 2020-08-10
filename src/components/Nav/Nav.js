7b28f1c611a71949d77f40baf438a6909d45d577
Josh Maus <jman985@gmail.com>
2020-08-10 14:29:45 -0500

    added nav bar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,35 +3,84 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+  MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBContainer } from "mdbreact";
 
-const Nav = (props) => (
-  <div className="nav">
-    <Link to="/home">
-      <h2 className="nav-title">Hackintosh Builder</h2>
-    </Link>
-    <div className="nav-right">
-      <Link className="nav-link" to="/home">
+
+  const Nav = (props) => (
+  // <div className="nav">
+  //   <Link to="/home">
+  //     <h2 className="nav-title">Hackintosh Builder</h2>
+  //   </Link>
+  //   <div className="nav-right">
+  //     <Link className="nav-link" to="/home">
+  //       {/* Show this link if they are logged in or not,
+  //       but call this link 'Home' if they are logged in,
+  //       and call this link 'Login / Register' if they are not */}
+  //       {props.user.id ? 'Home' : 'Login / Register'}
+  //     </Link>
+  //     {/* Show the link to the info page and the logout button if the user is logged in */}
+  //     {props.user.id && (
+  //       <>
+  //         <Link className="nav-link" to="/builds">
+  //           View Builds
+  //         </Link>
+  //         <LogOutButton className="nav-link"/>
+  //       </>
+  //     )}
+  //     {/* Always show this link since the about page is not protected */}
+  //     <Link className="nav-link" to="/about">
+  //       About
+  //     </Link>
+  //   </div>
+  // </div>
+
+  <MDBNavbar color="orange" dark expand="md" style={{ marginTop: "20px" }}>
+  <MDBNavbarBrand>
+    <strong className="white-text">Hackintosh Builder</strong>
+  </MDBNavbarBrand>
+  
+    <MDBNavbarNav left>
+      
+      <MDBNavItem>
+      <MDBNavLink to="/home">
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
         {props.user.id ? 'Home' : 'Login / Register'}
-      </Link>
+      </MDBNavLink >      
+      </MDBNavItem>
+
+      <MDBNavItem>
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
-        <>
-          <Link className="nav-link" to="/builds">
+        
+          <MDBNavLink to="/builds">
             View Builds
-          </Link>
-          <LogOutButton className="nav-link"/>
-        </>
+          </MDBNavLink >
+      
       )}
-      {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
-        About
-      </Link>
-    </div>
-  </div>
+      
+      <MDBNavLink to="/about">About</MDBNavLink>
+  </MDBNavItem>
+
+    </MDBNavbarNav>
+    {props.user.id && (
+      <>
+    <MDBNavbarNav right>
+      <MDBNavItem>
+        <LogOutButton/>
+      </MDBNavItem>
+      </MDBNavbarNav>
+      </>
+      )}
+</MDBNavbar>
+
 );
+
+
+
+
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
