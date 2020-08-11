12a68f48c1cf9c56b78348acb6eba401c2981f8d
Josh Maus <jman985@gmail.com>
2020-08-10 23:55:14 -0500

    added grid css

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -6,11 +7,20 @@ import React from 'react';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 const AboutPage = () => (
-  <div>
+  <div style={{ textAlign: 'center'}}>
+    <h1>Acknowledgements</h1>
     <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
+      <p >
+      Special thanks to Prime Academy, Paxos Instructors, and the entire Paxos Cohort.  Thank 
+      you to all Prime Academy students, staff, and alumni.
+  </p>
+    </div>
+
+    <div>
+    <p>
+        This project is strictly for INFORMATIONAL and EDUCATIONAL purposes ONLY.
+          Please note that installing Apple software on non-Apple hardware, while not illegal, 
+        violates the <Link to= "https://www.apple.com/legal/sla/ ">APPLE INC. SOFTWARE LICENSE AGREEMENT</Link> </p>
     </div>
   </div>
 );
