6eff8ac23a021540e7f71c7683ddae6a2367fb21
Josh Maus <jman985@gmail.com>
2020-08-19 23:46:34 -0500

    edited ReadME, added database

--- README.md
+++ README.md
@@ -14,27 +14,42 @@ To see the fully functional site, please visit: [DEPLOYED VERSION OF APP](www.he
 
 ## Screen Shot
 
-Include one or two screen shots of your project here (optional). Remove if unused.
+**Home Page View**
+![Home Page View](/public/images/homeView.png)
+
+
+![Builder View](/public/images/builderView.png)
+
+![Component View (CPU)](/public/images/componentView.png)
+
+![New/Edit Build View](/public/images/newBuildView.png)
+
+![User Builds View](/public/images/userBuildsView.png)
+
+![Review View](/public/images/reviewView.png)
 
 ### Prerequisites
 
-Link to software that is required to install the app (e.g. node).
+Before you get started, make sure you have the following software installed on your computer:
 
 - [Node.js](https://nodejs.org/en/)
-- List other prerequisites here
+- [PostrgeSQL](https://www.postgresql.org/)
+- [Nodemon](https://nodemon.io/)
 
 ## Installation
 
-How do you get your application up and running? This is a step by step list for how another developer could get this project up and running. The good target audience in terms of knowledge, would be a fellow Primer from another cohort being able to spin up this project. Note that you do not need a paragraph here to intro Installation. It should be step-by-step.
-
-If your application has secret keys (for example --  Twilio), make sure you tell them how to set that up, both in getting the key and then what to call it in the `.env` file.
-
-1. Create a database named `your database name`,
-2. The queries in the `tables.sql` file are set up to create all the necessary tables and populate the needed data to allow the application to run correctly. The project is built on [Postgres](https://www.postgresql.org/download/), so you will need to make sure to have that installed. We recommend using Postico to run those queries as that was used to create the queries, 
-3. Open up your editor of choice and run an `npm install`
-4. Run `npm run server` in your terminal
-5. Run `npm run client` in your terminal
-6. The `npm run client` command will open up a new browser tab for you!
+* Create a database named `prime_app`
+* The queries in the `database.sql` file are set up to create all the necessary tables and populate the needed data to allow the application to run correctly. The project is built on [Postgres](https://www.postgresql.org/download/), so you will need to make sure to have that installed. We recommend using Postico to run those queries as that was used to create the queries
+* Run `npm install`
+* Create a `.env` file at the root of the project and paste this line into the file:
+    ```
+    SERVER_SESSION_SECRET=superDuperSecret
+    ```
+    While you're in your new `.env` file, take the time to replace `superDuperSecret` with some long random string like `25POUbVtx6RKVNWszd9ERB9Bb6` to keep your application secure. Here's a site that can help you: [https://passwordsgenerator.net/](https://passwordsgenerator.net/). If you don't do this step, create a secret with less than eight characters, or leave it as `superDuperSecret`, you will get a warning.
+* Start postgres if not running already by using `brew services start postgresql`
+* Run `npm run server`
+* Run `npm run client`
+* The `npm run client` command will open up a new browser tab for you
 
 ## Usage
 How does someone use this application? Tell a user story here.
@@ -49,20 +64,20 @@ How does someone use this application? Tell a user story here.
 
 ## Built With
 
-- React
-- Redux
+- React.js
+- React-Redux
 - Redux-Sagas 
 - PostgreSQL
 - Passport
 - Node
-- Express
+- Express.js
 - React-Router-Dom
 - Material-UI
-- MDB React
+- Material Design for Bootstrap for React
 - ReactCardFlip
 
 ## Acknowledgement
 Thanks to [Prime Digital Academy](www.primeacademy.io) who equipped and helped me to make this application a reality.  Special thanks to the Paxos instructors and the entire Paxos cohort.
 
 ## Support
-If you have suggestions or issues, please email me at [youremail@whatever.com](www.google.com)
+If you have suggestions or issues, please email me at [jman985@gmail.com](www.google.com)
--- database.sql
+++ database.sql
@@ -5,148 +5,6 @@ CREATE TABLE "user" (
 );
 
 
-CREATE TABLE "cpu" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "cpu" ("name", "image", "details")
-VALUES 
-('CPU', 'images/Intel-logo.png' , 'CPU stands for "central processing unit".  MacOS runs natively only on Intel processors.'),
-('Intel Core i5-9400', 'images/i5-9400', '6 cores, 6 threads, up to 4.1 GHz max boost' ),
-('Intel Core i5-9600K', 'images/i5-9600K', '6 cores, 6 threads, up to 4.6 GHz max boost'),
-('Intel Core i7-9700', 'images/i5-9700', '8 cores, 8 threads, up to 4.7 GHz max boost'),
-('Intel Core i7-9700K','images/i5-9700K', '8 cores, 8 threads, up to 4.9 GHz max boost');
-
-
-CREATE TABLE "cooler" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "cooler" ("name", "image", "details")
-VALUES
-('CPU Cooler' , 'images/noctua_logo.jpg' , 'The CPU cooler sits on top of the CPU and keeps it from overheating.'),
-(  'Noctua NH-L9i', 'images/NH-L9i-cooler.jpg', '37 height' ),
-(  'NoctuaNH-L12S', 'images/NH-L12S-cooler.jpg', '70mm height' ),
-(  'Noctua NH-U9S','images/NH-U9S-cooler.jpg', '124mm height' );
-
-
-
-CREATE TABLE "motherboard" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "motherboard" ("name", "image", "details")
-VALUES 
-('Motherboard' , 'images/asus-logo.png' , 'Also called a PCB (printed circuit board) or chipset, the motherboard connects the components together.  Intel CPUs require Intel chipsets'),
-(  'ASRock Z390M-ITX','images/ASRock-Z390-mobo.jpg', 'Mini-ITX form factor; Intel Z390 chipset'),
-(  'ASRock Z390 Phantom Gaming-ITX','images/ASRock-Fatality-mobo.jpg', 'Mini-ITX form factor;  Intel Z390 chipset'),
-(  'ASUS ROG Strix Z390-I Gaming', 'images/ASUS-Z390-mobo.jpg', 'Mini-ITX form factor; Intel Z390 chipset'),
-(  'Gigabyte H370N WiFi', 'images/Gigabyte-H370N-mobo.jpg', 'Mini-ITX form factor; Intel H370 chipset');
-
-
-CREATE TABLE "memory" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "memory" ("name", "image", "details")
-VALUES 
-('Memory' , 'images/micron-logo.png' , 'Also called RAM or "random access memory", the CPU uses RAM to store program data for quick access'),
-( 'G.Skill Ripjaws V Series', 'images/G.Skill.V-memory.jpg', '16GB DDR4-3200MHz RAM' ),
-( 'G.Skill Ripjaws V Series', 'images/G.Skill.V32-memory.jpg', '32GB DDR4-3600MHz RAM' ),
-( 'TEAMGROUP T-FORCE Vulcan Z ', 'images/T-FORCE-VULCAN-memory.jpg', '16GB DDR4-2666MHz RAM' ),
-( 'Corsair Vengeance LPX', 'images/CORSAIR-LPX-memory.jpg', '32GB DDR4-3600MHz RAM' );
-
-
-CREATE TABLE "gpu" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "gpu" ("name", "image", "details")
-VALUES
-('GPU' , 'images/AMD_Radeon.png' , 'GPU stands for "Graphics Processing Unit".  MacOS runs native on AMD GPUs'),
-( 'Sapphire PULSE Radeon RX 580', 'images/Radeon-RX580.jpg', '8GB GDDR5 Memory; 1366 MHz max boost'),
-( 'MSI Radeon RX 5700 Evoke OC', 'images/MSI-EvokeOC-5700.jpg', '8GB GDDR6 Memory; 1700 MHz max boost'),
-( 'Sapphire PULSE Radeon RX 5600 XT', 'images/Pulse-5600xt.jpg', '6GB GDDR6 Memory; 1750 MHz max boost'),
-( 'Gigabyte Radeon RX 5700 XT Gaming OC', 'images/Gigabyte-5700xt.jpg', '8GB GDDR6 Memory; 1905 MHz max boost');
-
-
-CREATE TABLE "storage" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "storage" ("name", "image", "details")
-VALUES 
-('Storage' , 'images/samsung-logo.png' , 'Sometimes called a hard drive or solid-state drive (SSD), this stores data on the computer. Most real Macs use Samsung memory modules'),
-( 'Samsung 970 Evo Plus', 'images/970-Evo-Plus-500.jpg', '500GB NVMe M.2 PCIe SSD' ),
-( 'Samsung 970 Evo Plus', 'images/970-Evo-Plus-1000.jpg', '1TB NVMe M.2 PCIe SSD' ),
-( 'Samsung 970 Evo Plus', 'images/970-Evo-Plus-2000.jpg', '2TB NVMe M.2 PCIe SSD' );
-
-
-CREATE TABLE "case" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-INSERT INTO "case" ("name", "image", "details")
-VALUES 
-('Case' , 'images/nzxt-logo.jpg' , 'The case houses all of the components.'),
-(  'MITXPC MX500','images/MITXPC-case.jpg', 'Mini-ITX form factor; galvanized steel material; 2.7 liters'),
-(  'Fractal Design Node 202' , 'images/Node202.jpg', 'Mini-ITX form factor; galvanized steel and plastic; 10.2 liters' ),
-(  'CoolerMaster MasterBox NR200', 'images/NR200-case.jpg', 'Mini-ITX form factor; steel/plastic material; 18.25 liters' ),
-(  'Thermaltake Core V1','images/ThermaltakeV1.jpg', 'Mini-ITX form factor; steel/plastic material; 22.5 liters' );
-
-
-CREATE TABLE "psu" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "image" VARCHAR(2083),
-    "details" VARCHAR(2083)
-);
-
-
-INSERT INTO "psu" ("name", "image", "details")
-VALUES
-('Power Supply' , 'images/corsair-logo.png' , 'PSU stands for "power supply unit".  It converts power from the wall into power your computer can use.'),
-(  'Corsair SF600', 'images/SF600-PSU.jpg' , '600 Watt SFX 80+ Platinum rated power supply'),
-(  'Corsair SF750','images/SF750-PSU.jpg' , '750 Watt SFX 80+ Platinum rated power supply'),
-(  'EVGA SuperNOVA 550 GM', 'images/EVGA-550GM-PSU.jpg' , '550 Watt SFX 80+ Gold rated power supply'),
-(  'Corsair RM650', 'images/RM500-PSU.jpg' , '650 Watt ATX 80+ Gold rated power supply');
-
-
-CREATE TABLE "build" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL,
-    "user_id" INT REFERENCES "user"("id"),
-    "cpu_id" INT REFERENCES "cpu"("id"),
-    "mobo_id" INT REFERENCES "motherboard"("id"),
-    "storage_id" INT REFERENCES "storage"("id"),
-    "memory_id" INT REFERENCES "memory"("id"),
-    "cooler_id" INT REFERENCES "cooler"("id"),
-    "psu_id" INT REFERENCES "psu"("id"),
-    "case_id" INT REFERENCES "case"("id"),
-    "gpu_id" INT REFERENCES "gpu"("id")
-);
-
 CREATE TABLE "components" (
     "id" SERIAL PRIMARY KEY,
     "type" VARCHAR (100) NOT NULL,
@@ -156,4 +14,56 @@ CREATE TABLE "components" (
 );
 
 
+INSERT INTO "components" ("type", "name" , "image" , "details")
+VALUES ('CPU', 'CPU' , 'images/intel-logo2.jpg' , 'The CPU, or "Central Processing Unit" coordinates all programs and tasks, and is the "brains" of the computer.  MacOS runs natively only on Intel processors.'),
+		('CPU Cooler','CPU Cooler' , 'images/noctua_logo.jpg' , 'The CPU cooler sits on top of the CPU and keeps it from overheating.'),
+		('Motherboard','Motherboard' , 'images/asus-logo.png' , 'Also called a PCB (printed circuit board), the motherboard connects all the components together.'),
+		('Case' , 'Case' , 'images/nzxt-logo.jpg' , 'The case houses all of the components.'),
+		('GPU','GPU' , 'images/AMD_Radeon.jpg' , 'The GPU, or "Graphics Processing Unit", is used to process complex graphical data.  MacOS runs natively on AMD GPUs.'),
+		('Storage' ,'Storage' , 'images/samsung-logo.png' , 'Usually in the form of a hard drive or SSD (solid-state drive), this component is used to store long-term data. Most real Macs use Samsung-branded memory modules.'),
+		('Memory', 'Memory' , 'images/micron-logo.png' , 'Called RAM, or "Random Access Memory", the CPU uses this component to store short-term program data for quick access.'),
+		('Power Supply' , 'Power Supply' , 'images/corsairlogo2.png' , 'PSU stands for "Power Supply Unit".  It converts outlet power into power your computer can use.'),
+		('CPU','Intel Core i5-9400', 'images/i5-9400.jpg', '6 cores, 6 threads, up to 4.1 GHz max boost clock' ),
+		('CPU','Intel Core i5-9600K', 'images/i5-9600K.jpg', '6 cores, 6 threads, up to 4.6 GHz max boost clock'),
+		('CPU','Intel Core i7-9700', 'images/i5-9700.jpg', '8 cores, 8 threads, up to 4.7 GHz max boost clock'),
+		('CPU','Intel Core i7-9700K','images/i5-9700K.jpg', '8 cores, 8 threads, up to 4.9 GHz max boost clock'),
+		( 'CPU Cooler','Noctua NH-L9i', 'images/NH-L9i-cooler.jpg', 'Height:  37mm' ),
+		( 'CPU Cooler','NoctuaNH-L12S', 'images/NH-L12S-cooler.jpg', 'Height:  70mm' ),
+		( 'CPU Cooler','Noctua NH-U9S','images/NH-U9S-cooler.jpg', 'Height:  124mm' ),
+		(  'Motherboard','ASRock Z390M-ITX','images/ASRock-Z390-mobo.jpg', 'Mini-ITX form factor;   Intel Z390 chipset'),
+		(  'Motherboard','ASRock Z390 Phantom Gaming-ITX','images/ASRock-Fatality-mobo.jpg', 'Mini-ITX form factor;   Intel Z390 chipset'),
+		(  'Motherboard','ASUS ROG Strix Z390-I Gaming', 'images/ASUS-Z390-mobo.jpg', 'Mini-ITX form factor;   Intel Z390 chipset'),
+		(  'Motherboard','Gigabyte H370N WiFi', 'images/Gigabyte-H370N-mobo.jpg', 'Mini-ITX form factor;   Intel H370 chipset'),
+		(  'Case','Phanteks Enthoo Evolv ITX','images/Evolv-ITX-1.jpg', 'Mini-ITX form factor; Galvanized steel, plastic, and tempered glass;   2.7 liters'),
+		(  'Case','Fractal Design Node 202' , 'images/Node202.jpg', 'Mini-ITX form factor; Galvanized steel and plastic;   10.2 liters' ),
+		(  'Case','CoolerMaster MasterBox NR200', 'images/NR200-case.jpg', 'Mini-ITX form factor; Steel and plastic ;   18.25 liters' ),
+		(  'Case','Thermaltake Core V1','images/ThermaltakeV1.jpg', 'Mini-ITX form factor; Steel and plastic;   22.5 liters' ),
+		( 'GPU','Sapphire PULSE Radeon RX 580', 'images/Radeon-RX580.jpg', '8GB GDDR5 Memory;  1366 MHz max boost'),
+		( 'GPU','MSI Radeon RX 5700 Evoke OC', 'images/MSI-EvokeOC-5700.jpg', '8GB GDDR6 Memory;   1700 MHz max boost'),
+		( 'GPU','Sapphire PULSE Radeon RX 5600 XT', 'images/Pulse-5600xt.jpg', '6GB GDDR6 Memory;   1750 MHz max boost'),
+		( 'GPU','Gigabyte Radeon RX 5700 XT Gaming OC', 'images/Gigabyte-5700xt.png', '8GB GDDR6 Memory;   1905 MHz max boost'),
+		( 'Storage','Samsung 970 Evo Plus 500GB', 'images/970-Evo-Plus-500.jpg', '500GB NVMe M.2 PCIe SSD' ),
+		( 'Storage','Samsung 970 Evo Plus 1TB', 'images/970-Evo-Plus-1000.jpg', '1TB NVMe M.2 PCIe SSD' ),
+		( 'Storage','Samsung 970 Evo Plus 2TB', 'images/970-Evo-Plus-2000.jpg', '2TB NVMe M.2 PCIe SSD' ),
+		( 'Memory','G.Skill Ripjaws V Series 16GB', 'images/G.Skill.V-memory.jpg', '16GB DDR4-3200MHz RAM' ),
+		( 'Memory','G.Skill Ripjaws V Series 32GB', 'images/G.Skill.V32-memory.jpg', '32GB DDR4-3600MHz RAM' ),
+		( 'Memory','TEAMGROUP T-FORCE Vulcan Z 16GB', 'images/T-FORCE-VULCAN-memory.jpg', '16GB DDR4-2666MHz RAM' ),
+		( 'Memory','Corsair Vengeance LPX 32GB', 'images/CORSAIR-LPX-memory.jpg', '32GB DDR4-3600MHz RAM' ),
+		(  'Power Supply','Corsair SF600', 'images/SF600-PSU.jpg' , '600 Watt SFX 80+ Platinum rated power supply'),
+		(  'Power Supply','Corsair SF750','images/SF750-PSU.jpg' , '750 Watt SFX 80+ Platinum rated power supply'),
+		(  'Power Supply','EVGA SuperNOVA 550 GM', 'images/EVGA-550GM-PSU.jpg' , '550 Watt SFX 80+ Gold rated power supply'),
+		(  'Power Supply','Corsair RM650', 'images/RM500-PSU.jpg' , '650 Watt ATX 80+ Gold rated power supply');
 
+
+CREATE TABLE "build" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (100),
+    "user_id" INT REFERENCES "user"("id"),
+    "cpu_id" INT REFERENCES "components"("id"),
+    "cooler_id" INT REFERENCES "components"("id"),
+    "mobo_id" INT REFERENCES "components"("id"),
+    "case_id" INT REFERENCES "components"("id"),
+    "gpu_id" INT REFERENCES "components"("id"),
+    "storage_id" INT REFERENCES "components"("id"),
+    "memory_id" INT REFERENCES "components"("id"),
+    "psu_id" INT REFERENCES "components"("id"));
