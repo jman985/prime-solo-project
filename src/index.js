0bcb1cc58521d720798ba9f7bbace70bf259c85d
Josh Maus <jman985@gmail.com>
2020-08-20 23:28:52 -0500

    deploy to heroku

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
 import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
 
 import App from './components/App/App';
-import 'index.css'
+// import './fonts/index.css'
 
 const sagaMiddleware = createSagaMiddleware();
 
